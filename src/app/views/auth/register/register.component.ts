87f59212f8b46b45fb527dac077cd00ae3d14e99
thevictorgreen <thevictorgreen@gmail.com>
2018-10-08 07:48:19 -0400

    Registration update

--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { AccountService } from '../../../services/account.service';
+import { StripeService } from '../../../services/stripe.service';
 
 @Component({
   selector: 'app-dashboard',
